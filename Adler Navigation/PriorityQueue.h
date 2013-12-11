69bf92d743fee98dce2023a040ca4615151adbc7
Ahaan Ugale <ugale2@illinois.edu>
2013-12-11 01:48:53 -0600

    Added simple array based priority queue implementation.

---
+++ Adler Navigation/PriorityQueue.h
@@ -0,0 +1,15 @@
+//
+//  PriorityQueue.h
+//  Adler Navigation
+//
+//  Created by Ahaan Ugale on 12/11/13.
+//  Copyright (c) 2013 Adler Planetarium. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PriorityQueue : NSObject
+
+- (void)addItem:(NSObject *)item withPriority:(float)priority;
+
+@end
---
+++ Adler Navigation/PriorityQueue.m
@@ -0,0 +1,53 @@
+//
+//  PriorityQueue.m
+//  Adler Navigation
+//
+//  Created by Ahaan Ugale on 12/11/13.
+//  Copyright (c) 2013 Adler Planetarium. All rights reserved.
+//
+
+#import "PriorityQueue.h"
+
+@interface Item : NSObject
+@property (nonatomic, strong) NSObject *obj;
+@property (nonatomic) float priority;
+@end
+
+@implementation PriorityQueue
+{
+    NSMutableArray *items;
+    int startIdx;
+}
+
+
+- (id)init
+{
+    self = [super init];
+    if (self)
+    {
+        items = [[NSMutableArray alloc] init];
+        startIdx = 0;
+    }
+    return self;
+}
+
+- (void)addItem:(NSObject *)obj withPriority:(float)priority
+{
+    int i = startIdx;
+    while (i < [items count] && ((Item *)[items objectAtIndex:i]).priority < priority ) {
+        i++;
+    }
+    
+    Item *item = [[Item alloc] init];
+    item.obj = obj;
+    item.priority = priority;
+    
+    [items insertObject:item atIndex:i];
+}
+
+- (NSObject *)getItemLeastPriority
+{
+    return ((Item *)[items objectAtIndex:startIdx++]).obj;
+}
+
+@end
