157cc7b79c9d7c9bcb6d03b24cea06efe33af167
Ahaan Ugale <ugale2@illinois.edu>
2013-12-11 02:14:04 -0600

    Added tests for dijkstra

--- Adler NavigationTests/MapViewControllerTests.m
+++ Adler NavigationTests/MapViewControllerTests.m
@@ -12,6 +12,7 @@
 #import "ExitNode.h"
 #import "TravelNode.h"
 #import "ExhibitNode.h"
+#import "MapGraph.h"
 
 @interface MapViewControllerTests : XCTestCase
 
@@ -48,6 +49,67 @@
     NSArray *path = @[n1, n2, n3];
     
     [MapViewController nextDirection:path];
+}
+
+- (void)testDijkstra
+{
+    MapGraph *g;
+    Node *n1;
+    Node *n2;
+    Node *n3;
+    Node *n4;
+    Edge *e12;
+    Edge *e13;
+    Edge *e34;
+    
+    [super setUp];
+    
+    g = [[MapGraph alloc] init];
+    
+    n1 = [[Node alloc] init];
+    n1.xCoord = 1;
+    n1.yCoord = 1;
+    
+    n2 = [[Node alloc] init];
+    n2.xCoord = 2;
+    n2.yCoord = 13;
+    
+    n3 = [[Node alloc] init];
+    n3.xCoord = 2.5;
+    n3.yCoord = 7;
+    
+    n4 = [[Node alloc] init];
+    n4.xCoord = 4;
+    n4.yCoord = 14;
+    
+    [g addNode:n1];
+    [g addNode:n2];
+    [g addNode:n3];
+    [g addNode:n4];
+    
+    e12 = [[Edge alloc]init];
+    e12.node1 = n1;
+    e12.node2 = n2;
+    
+    e13 = [[Edge alloc]init];
+    e13.node1 = n3;
+    e13.node2 = n1;
+    
+    e34 = [[Edge alloc] init];
+    e34.node1 = n3;
+    e34.node2 = n4;
+    
+    [g addEdge:e12];
+    [g addEdge:e13];
+    [g addEdge:e34];
+    
+    NSArray *path = [MapViewController dijkstra:g from:n1 to:n3];
+    NSArray *expected = @[n1, n3];
+    XCTAssertEqual(path, expected);
+    
+    path = [MapViewController dijkstra:g from:n1 to:n4];
+    expected = @[n1, n3, n4];
+    XCTAssertEqual(path, expected);
 }
 
 @end
