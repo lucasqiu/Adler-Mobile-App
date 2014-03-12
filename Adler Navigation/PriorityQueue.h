39b403552d16cb4ec18f02352ebca605d5d42958
Lucas Qiu <qiushi1992@gmail.com>
2014-03-12 15:13:21 -0500

    Modified PriorityQueue.h, Added comparable.h for comparison of objects in PQ

--- Adler Navigation/PriorityQueue.h
+++ Adler Navigation/PriorityQueue.h
@@ -7,9 +7,31 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "comparable.h"
 
 @interface PriorityQueue : NSObject
 
-- (void)addItem:(NSObject *)item withPriority:(float)priority;
+#pragma mark - Init
+- (id)init;
+- (id)initWithObjects:(NSSet *)objects;
+- (id)initWithCapacity:(int)capacity;
+- (id)initWithCapacity:(int)capacity andType:(Class)oType; //Queue will reject objects not of that type
+
+#pragma mark - Request info
+- (BOOL)isEmpty;
+- (BOOL)contains:(id<comparable, NSObject>)object;
+- (Class)typeOfAllowedObjects; //Returns the type of objects allowed to be stored in the queue
+- (int) size;
+- (id)peek;
+- (id)poll;
+- (id)objectMatchingObject:(id<comparable, NSObject>)object;
+- (NSArray *)toArray;
+- (void)print;
+
+#pragma mark - Mutation
+- (void)clear;
+- (BOOL)add:(id<comparable, NSObject>)object;
+- (void)addWithPriority:(NSObject *)item Priority:(float)priority;
+- (void)remove:(id<comparable, NSObject>)object;
 
 @end
