faf28ed887de91bd11766f8770e25f2f59697ad1
Ahaan Ugale <ugale2@illinois.edu>
2014-03-16 16:06:42 -0500

    Changed nextDirection

--- Adler Navigation/MapViewController.m
+++ Adler Navigation/MapViewController.m
@@ -45,39 +45,24 @@
  * Draws the next direction and places the text direction on the view.
  * Removes the used Nodes from path.
  */
-- (void)nextDirection:(NSArray *)path
+- (void)nextDirection:(NSMutableArray *)path
 {
-    NSString *text = @"";
-    Node *start = [path firstObject];
-
+    Node *node1 = [path firstObject];
+    Node *node2 = [path objectAtIndex:1];
+    [path removeObjectAtIndex:0];
+    
     // change image based on zCoord
     
-    CGPoint points[[path count]];
+    CGPoint points[2];
+    points[0].x = node1.xCoordinate;
+    points[0].x = node1.yCoordinate;
+    points[1].x = node2.xCoordinate;
+    points[1].x = node2.yCoordinate;
     
-    unsigned i;
-    for (i = 1; i < [path count]; i++) {
-        Node *cur = [path objectAtIndex:i];
-        //[MapViewController drawDirectionFrom:[path objectAtIndex:i-1] to:cur];
-        points[i-1].x = cur.xCoord;
-        points[i-1].y = cur.yCoord;
-        
-        if ([cur class] == [Node class]) {
-            Node *exit = (Node *) cur;
-            //NSString *nextRoom = [start inRoom:exit.room1] ? exit.room2 : exit.room1;
-            //text = [text stringByAppendingString:[NSString stringWithFormat:@"Go into the %@", nextRoom] ];
-            // need modification 
-            break;
-        }
-    }
+    [self drawLineSegments:points count:2];
     
-    [self drawLineSegments:points count:i];
-    
-    //path = path objectsAtIndexes:[i..[path count];
-}
-
-+ (void)drawDirectionFrom:(Node *)n1 to:(Node *)n2
-{
-    
+    NSString *text = [NSString stringWithFormat:@"Go towards %@", node2.id];
+    //display text
 }
 
 - (void)drawLineSegments:(CGPoint *)points count:(size_t)count
@@ -95,6 +80,8 @@
     
 	// set stroking color and draw lines
 	[[UIColor redColor] setStroke];
+    
+    CGContextSetLineWidth(ctx, 5.0f);
     
     CGContextStrokeLineSegments(ctx, points, count);
     
